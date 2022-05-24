7cb14ef9916bb55539110db3de97bb6a75d3c2db
Duck1s <joeri.breedveld123@gmail.com>
2022-05-24 20:16:49 +0200

    add: finish react-basics

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ import ReactDom from 'react-dom'
 
 import './index.css'
 
-const author = 'Amelia Hepworth'
-const title = 'I Love You to the Moon and Back'
-const img =
-	'https://wwwmpa.mpa-garching.mpg.de/gadget/cr/CR_TCDM_dump40_400_170000_12000_100_blue.gif'
+const firstBook = {
+	author: 'Amelia Hepworth',
+	title: 'I Love You to the Moon and Back',
+	img: 'http://balkadu.com/wp-content/uploads/2017/07/400X400.jpg',
+}
+
+const secondBook = {
+	author: 'Book 2',
+	title: 'Kaas',
+	img: 'https://i.pinimg.com/originals/82/e6/7c/82e67c686a6f80321455b3d016def273.jpg',
+}
 
 function BookList() {
 	return (
 		<section className='booklist'>
-			<Book title={title} />
+			<Book
+				title={firstBook.title}
+				img={firstBook.img}
+				author={firstBook.author}
+			/>
+			<Book
+				title={secondBook.title}
+				img={secondBook.img}
+				author={secondBook.author}
+			/>
 		</section>
 	)
 }
@@ -20,9 +36,9 @@ const Book = (props) => {
 	console.log(props)
 	return (
 		<article className='book'>
-			<img src={img} alt='' />
+			<img src={props.img} alt='' />
 			<h1>{props.title}</h1>
-			<h4>{author}</h4>
+			<h4>{props.author}</h4>
 		</article>
 	)
 }

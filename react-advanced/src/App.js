1b33ce80be17d870b26681300f2b0bd848bc5730
Duck1s <joeri.breedveld123@gmail.com>
2022-05-24 22:41:18 +0200

    add: usestate error, usestate basics, usestate array

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
+
 function App() {
-  return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
-    </div>
-  )
+	return (
+		<div className='container'>
+			<Setup />
+		</div>
+	)
 }
 
 export default App
--- react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+	// console.log(useState('hello world'))
+	// const value = useState(1)[0]
+	// const handler = useState(1)[1]
+	// console.log(value, handler)
+	const [text, setText] = useState('cheese')
 
-export default UseStateBasics;
+	const handleClick = () => {
+		if (text === 'cheese') {
+			setText('kaas')
+		} else {
+			setText('cheese')
+		}
+	}
+
+	return (
+		<React.Fragment>
+			<h1>{text}</h1>
+			<button className='btn' onClick={handleClick}>
+				change title
+			</button>
+		</React.Fragment>
+	)
+}
+
+export default UseStateBasics

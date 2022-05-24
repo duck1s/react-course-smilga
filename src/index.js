4d52c8762be2f1572bbddfb55009c5f84bdc9cd3
Duck1s <joeri.breedveld123@gmail.com>
2022-05-24 16:03:30 +0200

    add: props, variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Greeting() {
-	return <h4>this is joeri and this is my first component</h4>
+import './index.css'
+
+const author = 'Amelia Hepworth'
+const title = 'I Love You to the Moon and Back'
+const img =
+	'https://wwwmpa.mpa-garching.mpg.de/gadget/cr/CR_TCDM_dump40_400_170000_12000_100_blue.gif'
+
+function BookList() {
+	return (
+		<section className='booklist'>
+			<Book title={title} />
+		</section>
+	)
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+const Book = (props) => {
+	console.log(props)
+	return (
+		<article className='book'>
+			<img src={img} alt='' />
+			<h1>{props.title}</h1>
+			<h4>{author}</h4>
+		</article>
+	)
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'))
